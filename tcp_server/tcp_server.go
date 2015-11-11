5cc5a1279562f617e79b00651fbb2b3fe993f75a
rodr <ohrodr@gmail.com>
2015-11-10 21:59:01 -0800

    added new test

---
+++ tcp_server/tcp_server.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+import "net"
+import "os"
+
+func main() {
+	listen_sock, err := net.Listen("tcp", ":2888")
+	recv_chan := make(chan byte)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer listen_sock.Close()
+
+	for {
+		conn, err := listen_sock.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		go handleConnection(conn, recv_chan)
+		select {
+		case r := <-recv_chan:
+			fmt.Println(string(r))
+		}
+	}
+}
+
+func handleConnection(c net.Conn, out chan byte) {
+	defer c.Close()
+	fmt.Println("connection found")
+	buf := make([]byte, 1024)
+	readLength, err := c.Read(buf)
+	fmt.Println(fmt.Sprintf("bytes read: %d", readLength))
+	if err != nil {
+		fmt.Println("Error reading:", err.Error())
+	}
+	c.Write([]byte("Message received."))
+	for _, i := range buf {
+		out <- i
+	}
+
+}
